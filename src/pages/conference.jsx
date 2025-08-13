1277b71e39b1b8033bdf65f8eb20c442683f48aa
YARRACHANDU <yarrachandu7@gmail.com>
2025-08-13 11:36:16 +0530

    chandu

--- src/pages/conference.jsx
+++ src/pages/conference.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import Homenav from "../components/homepagecomponents/homenav";
 import Footer from "../components/homepagecomponents/homefooter";
-import Important from "../components/ConferenceCommitte/Important"
-import TimeLine from "../components/ConferenceCommitte/TimeLine"
+import Important from "../components/ConferenceCommitte/Important";
+import TimeLine from "../components/ConferenceCommitte/TimeLine";
 
 function Conference() {
   return (
     <div>
       <Homenav />
-<<<<<<< HEAD
       <div style={{ marginTop: "120px" }}>
         <TimeLine />
         <Important />
       </div>
-=======
-      <TimeLine/>
-      <Important/>
->>>>>>> 1ba62c1aefd9eea14789b673847c900f8ee4b4f2
+
       <Footer />
     </div>
   );
